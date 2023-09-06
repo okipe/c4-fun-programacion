2c0de06c8df0191c565c05bfb13f79dad08e6d4f
Oscar Roman <oscarroman.pe@gmail.com>
2023-09-06 17:01:33 -0500

    First example not completed

---
+++ interactivos/interactivo-1/Impresora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases_Esquema
+
+{
+    class Constructor
+    {
+        int varable_1;
+        string variable_2;
+
+        public Constructor(int varable_1, string variable_2)
+        {
+            this.varable_1 = varable_1;
+            this.variable_2 = variable_2;
+        }
+    }
+    internal static class Impresora
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
